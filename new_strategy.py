2c8923ae40350765f376f871f9adf0382f229632
chamchipack <chamchipack@chamchipackui-iMac.local>
2026-06-21 09:42:16 +0900

    gg

--- new_strategy.py
+++ new_strategy.py
@@ -21,6 +21,8 @@ def get_macro_data():
         "나스닥": "^IXIC",
         "나스닥 선물": "NQ=F",
         "필라델피아 반도체": "^SOX",
+        "마이크론: "MU",
+        "MSCI한국": "EWY"
     }
     results = []
     for name, ticker in macro_tickers.items():
